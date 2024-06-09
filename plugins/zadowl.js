8ee5131aa8ec7c76ac0daba11a8180af689c1574
kingibrahimtech <161978665+kingibrahimtech@users.noreply.github.com>
2024-06-09 11:33:02 +0300

    Update zadowl.js

--- plugins/zadowl.js
+++ plugins/zadowl.js
@@ -252,7 +252,7 @@ cmd({
     const _0x4ac8f2 = await search(_0x1cb962);
     if (_0x4ac8f2.length) {
       let _0x3d85b = await download(_0x4ac8f2[0x0].id);
-      let _0x307e6f = "*sᴜʜᴀɪʟ-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ* \n*________________________________* \n\n*_Reply Any Number To Download._*\n_Results For : " + _0x1cb962 + "_ \n";
+      let _0x307e6f = "*Black Panther • Apps* \n*________________________________* \n\n*_Reply Any Number To Download._*\n_Results For : " + _0x1cb962 + "_ \n";
       for (let _0x5a5920 = 0x0; _0x5a5920 < _0x4ac8f2.length; _0x5a5920++) {
         _0x307e6f += "\n*" + (_0x5a5920 + 0x1) + " : " + _0x4ac8f2[_0x5a5920].name + "* \n*Id : " + _0x4ac8f2[_0x5a5920].id + "* \n";
       }
@@ -280,7 +280,7 @@ smd({
 }, async (_0x1ae8f8, _0x1c586e) => {
   try {
     if (!_0x1c586e) {
-      return await _0x1ae8f8.reply("*Provide Repo Url, _.gitclone https://github.com/SuhailTechInfo/Suhail-Md_*");
+      return await _0x1ae8f8.reply("*Provide Repo Url, _.gitclone https://github.com/Kingibrahimtech/Black-panther-md-x_*");
     }
     if (!/(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i.test(_0x1c586e)) {
       return await _0x1ae8f8.reply("*Provide Valid Repositry Url*");
@@ -770,7 +770,7 @@ smd({
     }
     let _0x3b2ca6 = await yts(_0x4ec99f);
     let _0x4123ae = _0x3b2ca6.all[0x0];
-    let _0x5883a9 = "\t *sᴜʜᴀɪʟ-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 To Video_ Or _1 document_\n_Reply 2 To Audio_ Or _2 document_";
+    let _0x5883a9 = "\t * "Black Panther • Songs*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 To Video_ Or _1 document_\n_Reply 2 To Audio_ Or _2 document_";
     let _0x3885cc = await smdBuffer(_0x4123ae.thumbnail);
     var _0x44a363 = {
       ...(await _0x2c2023.bot.contextInfo(Config.botname, "ʏᴏᴜᴛᴜʙᴇ ꜱᴏɴɢ", _0x3885cc))
@@ -797,7 +797,7 @@ cmd({
       return await _0x1c8285.reply("*_Give Me Search Query!_*");
     }
     let _0x2878ec = await yts(_0xca939c);
-    let _0x4186e4 = "*sᴜʜᴀɪʟ-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
+    let _0x4186e4 = "*Black Panther • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
     let _0x463366 = 0x1;
     for (let _0x308e22 of _0x2878ec.all) {
       _0x4186e4 += " \n*" + _0x463366++ + " : " + _0x308e22.title + (_0x308e22.timestamp ? '(' + _0x308e22.timestamp + ')' : '') + "*\n*Url : " + _0x308e22.url + '*';
@@ -1036,7 +1036,7 @@ cmd({
 }) => {
   if (_0xb75e78.quoted && _0xb75e78.text) {
     const _0x5b8ee5 = _0xb75e78.quoted.text.split("\n");
-    if (_0x5b8ee5[0x0].includes("sᴜʜᴀɪʟ-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ")) {
+    if (_0x5b8ee5[0x0].includes("Black Panther • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ")) {
       const _0x1724ba = _0x5b8ee5.find(_0x525632 => _0x525632.startsWith("Url :"));
       let _0x43a95e = _0x1724ba.replace("Url :", '').trim();
       try {
@@ -1092,7 +1092,7 @@ cmd({
         return await _0xb75e78.reply("Error While Downloading Video : " + _0x189dd8);
       }
     } else {
-      if (_0x5b8ee5[0x0].includes("sᴜʜᴀɪʟ-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ")) {
+      if (_0x5b8ee5[0x0].includes("Black Panther • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ")) {
         let _0x307bb6 = '*' + _0xb75e78.text.split(" ")[0x0] + " : ";
         const _0x56275d = _0x5b8ee5.find(_0x3b5e74 => _0x3b5e74.startsWith(_0x307bb6));
         if (_0x56275d) {
@@ -1133,7 +1133,7 @@ cmd({
           }
         }
       } else {
-        if (_0x5b8ee5[0x0].includes("sᴜʜᴀɪʟ-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ")) {
+        if (_0x5b8ee5[0x0].includes("Black Panther • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ")) {
           let _0x35d668 = '*' + _0xb75e78.text.split(" ")[0x0] + " : ";
           const _0x205a5f = _0x5b8ee5.find(_0x304058 => _0x304058.startsWith(_0x35d668));
           if (_0x205a5f) {
